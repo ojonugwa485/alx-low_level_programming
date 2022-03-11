5fca56cb902cdbd67376033c22034787ed0d82b0
ojonugwa485 <jefferyegbunu@gmail.com>
2022-03-10 17:44:37 -0800

    jeff

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - should "print the size of data types to the standard output"
+ *
+ * warning Allowed
+ *Return: return 0
+ */
+
+
+
+int main(void)
+
+{
+
+		printf("Size of a char: %zu byte(s)\n", sizeof(char));
+
+		printf("Size of an int: %zu byte(s)\n", sizeof(int));
+
+		printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+
+		printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+
+		printf("Size of a float: %zu byte(s)\n", sizeof(float));
+
+		return (0);
+
+}
