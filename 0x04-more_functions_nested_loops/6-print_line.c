58ceca1cebaf38e63ffe693a13a993c886bd2aeb
ojonugwa485 <jefferyegbunu@gmail.com>
2022-03-17 09:59:35 -0700

    jeffrey

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_line - Write a function that print a straight line
+ * @n: Integer amount of lines
+ */
+void print_line(int n)
+{
+char c;
+for (c = 0; c < n; c++)
+_putchar('_');
+_putchar('\n');
+}
