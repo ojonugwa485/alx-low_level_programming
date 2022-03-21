ed42fc8db76857177d63b7d0cc4080d489c694d1
ojonugwa485 <jefferyegbunu@gmail.com>
2022-03-21 15:48:37 -0700

    jeffrey

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - print n digits of an array
+ * @a: array to print
+ * @n: number of values to print
+ * Description: [rints n digits of array
+ * Return: nothing
+ **/
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+
+		printf("%d", a[i]);
+		if (i != n - 1)
+			printf(", ");
+	}
+
+	printf("\n");
+}
